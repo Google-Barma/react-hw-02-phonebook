1893d9bf3ac94c2ad6562a668a3c50439606fdd3
Vadym <vad.evlanov@gmail.com>
2020-11-27 11:55:38 +0200

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import { Component } from 'react';
+export default class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  render() {
+    return <div>test</div>;
+  }
+}
